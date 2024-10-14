e13163d84827abbbf259fd9a03e48e9cd8a84d53
Caio Escorel <caioheraclio@gmail.com>
2024-10-14 14:06:11 -0300

    Atualização de correções

--- biblioteca.py
+++ biblioteca.py
@@ -3,48 +3,60 @@ class Pessoa:
         self.nome = nome
         self.idade = idade
         self.peso = peso
-        self.fala = False
-        self.sono = False
-        self.fome = False
+        self.falando = False
+        self.dormindo = False
+        self.comendo = False
 
     def falar(self):
-        if self.sono == False and self.fome == False and self.fala == False:
-            print(f"{self.nome} foi falar")
-            self.fala = True
-        elif self.sono == False and self.fome == True:
-            print(f"{self.nome} Não pode falar, pois está comendo.")
-        elif self.fala == True:
-            print(f"{self.nome} já está falando")
+        if self.dormindo == False and self.comendo == False and self.falando == False:
+            print(f"{self.nome} foi falar.")
+            self.falando = True
+        elif self.dormindo == False and self.comendo == True:
+            print(f"{self.nome} não pode falar, pois está comendo.")
+        elif self.falando == True:
+            print(f"{self.nome} já está falando.")
         else:
-            print(f"{self.nome} Não pode falar, pois está dormindo")
+            print(f"{self.nome} não pode falar, pois está dormindo.")
 
     def dormir(self):
-        if self.fala == False and self.fome == False and self.sono == False:
-            print(f"{self.nome} foi dormir")
-            self.sono = True
-        elif self.fala == True and self.fome == False:
-            print(f"{self.nome} Não pode dormir, pois está dormindo")
-        elif self.sono == True:
-            print(f"{self.nome} já está dormindo")
+        if self.falando == False and self.comendo == False and self.dormindo == False:
+            print(f"{self.nome} foi dormir.")
+            self.dormindo = True
+        elif self.falando == True and self.comendo == False:
+            print(f"{self.nome} não pode dormir, pois está falando.")
+        elif self.dormindo == True:
+            print(f"{self.nome} já está dormindo.")
         else:
-            print(f"{self.nome} Não pode dormir, pois está comendo")
+            print(f"{self.nome} não pode dormir, pois está comendo.")
 
     def comer(self):
-        if self.sono == False and self.fala == False and self.fome == False:
-            print(f"{self.nome} foi comer")
-            self.fome = True
-        elif self.sono == False and self.fala == True:
-            print(f"{self.nome} não pode comer, pois está falando")
-        elif self.fome == True:
-            print(f"{self.nome} já está comendo")
+        if self.dormindo == False and self.falando == False and self.comendo == False:
+            print(f"{self.nome} foi comer.")
+            self.comendo = True
+        elif self.dormindo == False and self.falando == True:
+            print(f"{self.nome} não pode comer, pois está falando.")
+        elif self.comendo == True:
+            print(f"{self.nome} já está comendo.")
         else:
-            print(f"{self.nome} não pode comer, pois está dormindo")
+            print(f"{self.nome} não pode comer, pois está dormindo.")
 
     def pararDeComer(self):
-        self.fome = False
+        if self.comendo == True:
+            self.comendo = False
+            print(f"{self.nome} parou de comer.")
+        else:
+            print(f"Não tem como {self.nome} parar de comer, pois não está comendo.")
 
     def pararDeDormir(self):
-        self.sono = False
+        if self.comendo == True:
+            self.dormindo = False
+            print(f"{self.nome} parou de dormir.")
+        else:
+            print(f"Não tem como {self.nome} parar de dormir, pois não está dormindo.")
 
     def pararDeFalar(self):
-        self.fala = False
+        if self.falando == True:
+            self.falando = False
+            print(f"{self.nome} parou de falar.")
+        else:
+            print(f"Não tem como {self.nome} parar de falar, pois não está falando.")
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from biblioteca import *
 
 p1 = Pessoa("Caio", 19, 82)
 
+p1.falar()
+p1.pararDeFalar()
+p1.falar()
 p1.comer()
-p1.pararDeComer()
-p1.comer()
+p1.dormir()
+p1.pararDeDormir()
 
 
 
