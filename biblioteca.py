63eead0d28ead35f7f22ecbd270d749e0034c143
Caio Escorel <caioheraclio@gmail.com>
2024-10-14 16:41:50 -0300

    Exercícios de Herança

--- biblioteca.py
+++ biblioteca.py
@@ -60,3 +60,105 @@ class Pessoa:
             print(f"{self.nome} parou de falar.")
         else:
             print(f"Não tem como {self.nome} parar de falar, pois não está falando.")
+
+class Animal():
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+    def comer(self):
+        print(f"{self.nome} foi comer...")
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    def miar(self):
+        print(f"{self.nome} foi miando...")
+
+class Vaca(Animal):
+    def __init__(self,nome, cor):
+        super().__init__(nome, cor)
+    def mugir(self):
+        print(f"{self.nome} foi mugindo...")
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    def latir(self):
+        print(f"{self.nome} foi latindo...")
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+    def chiar(self):
+        print(f"{self.nome} foi chiando...")
+
+class Atleta():
+    def __init__(self,nome, peso):
+        self.nome = nome
+        self.aposentado = False
+        self.peso = peso
+        self.aquecido = False
+    def aposentar(self):
+        if self.aposentado == False:
+            print(f"O atleta {self.nome} se aposentou.")
+            self.aposentado = True
+        else:
+            print(f"O atleta {self.nome} não pode se aposentar, pois já é aposentado")
+    def aquecer(self):
+        if self.aposentado == False and self.aquecido == False:
+            print(f"O atleta {self.nome} está aquecendo!")
+            self.aquecido = True
+        elif self.aquecido == True:
+            print(f"O atleta {self.nome} não vai aquecer, pois já está aquecido.")
+        else:
+            print(f"O atleta {self.nome} está aposentado e não pode aquecer.")
+
+    def desaposentar(self):
+        if self.aposentado == True:
+            self.aposentado = False
+            print(f"{self.nome} não está mais aposentado.")
+        else:
+            print(f"{self.nome} já não é aposentado!")
+
+class Corredor(Atleta):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+    def correr(self):
+        if self.aposentado == True:
+            print(f"{self.nome} não pode correr, pois é aposentado.")
+        elif self.aquecido == True:
+            print(f"{self.nome} foi correr.")
+        else:
+            print(f"{self.nome} precisa aquecer antes de correr")
+
+class Nadador(Atleta):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+    def nadar(self):
+        if self.aposentado == True:
+            print(f"{self.nome} não pode nadar, pois é aposentado.")
+        elif self.aquecido == True:
+            print(f"{self.nome} foi nadar.")
+        else:
+            print(f"{self.nome} precisa aquecer antes de nadar")
+
+class Ciclista(Atleta):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+    def pedalar(self):
+        if self.aposentado == True:
+            print(f"{self.nome} não pode pedalar, pois é aposentado.")
+        elif self.aquecido == True:
+            print(f"{self.nome} foi pedalar.")
+        else:
+            print(f"{self.nome} precisa aquecer antes de pedalar")
+
+class TriAtleta(Corredor, Nadador, Ciclista):
+    def __init__(self, nome, peso):
+        super().__init__(nome, peso)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 from biblioteca import *
 
-p1 = Pessoa("Caio", 19, 82)
+"""p1 = Pessoa("Caio", 19, 82)
 
 p1.falar()
 p1.pararDeFalar()
-p1.falar()
-p1.comer()
-p1.dormir()
-p1.pararDeDormir()
+p1.comer()"""
+
+"""g1 = Gato("Branca", "branco")
+g1.comer()
+g1.miar()
+
+c1 = Coelho("Caio", "Marrom")
+c1.comer()
+c1.chiar()
+
+ca1 = Cachorro("Leleco", "Caramelo")
+ca1.comer()
+ca1.latir()
+
+v1 = Vaca("Loló", "preta")
+v1.comer()
+v1.mugir()"""
+
+
+
+tri = TriAtleta("Caio", 82)
+
+tri.aposentar()
+tri.aquecer()
+tri.desaposentar()
+tri.desaposentar()
+tri.aquecer()
+tri.correr()
+
 
 
 
